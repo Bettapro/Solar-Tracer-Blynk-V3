65c34a0ea80bbe0b2f5113a2a4fdb8d485c3b663
Alberto Bettin <albertobettin@live.com>
2025-10-24 08:42:49 +0200

    Default sync is not MQTT

--- SolarTracerBlynk/config.h
+++ SolarTracerBlynk/config.h
@@ -223,7 +223,7 @@
  * BLYNK
  * Specify how to connect to blynk server and when to sync the data from the tracer
  */
-#define USE_BLYNK
+//#define USE_BLYNK
 #ifdef USE_NOT_BLYNK
   #undef USE_BLYNK
 #endif
@@ -320,7 +320,7 @@
  * NOTE: mqtt support is till a WIP, it supports a small set of the features and variable compared
  *        to blynk. Developed and tested using a thingspeak server and mosquitto broker
  */
-//#define USE_MQTT
+#define USE_MQTT
 #ifdef USE_NOT_MQTT
   #undef USE_MQTT
 #endif
